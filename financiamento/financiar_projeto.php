7959bef66131fa7e9e4a805909b2f69c75410455
GeraldoOliveira <gevo.neto@hotmail.com>
2016-11-23 22:54:15 -0200

    Certo

--- financiamento/financiar_projeto.php
+++ financiamento/financiar_projeto.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta charset="utf-8"> 
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
         <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
